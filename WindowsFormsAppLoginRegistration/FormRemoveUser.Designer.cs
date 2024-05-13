d9f5420b88f6979b01302e572051e07ff19c7f8e
Dristy <Dristy@LAPTOP-7VVT9J9P>
2024-05-14 04:33:42 +0600

    Add project files.

---
+++ WindowsFormsAppLoginRegistration/FormRemoveUser.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsAppLoginRegistration
+{
+    partial class FormRemoveUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btbBack = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userGrid = new System.Windows.Forms.DataGridView();
+            this.UserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Role = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.userGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.userGrid);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btbBack);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnAddUser);
+            this.panel1.Controls.Add(this.txtUsername);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(643, 429);
+            this.panel1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(250, 19);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 33);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Remove User";
+            // 
+            // btbBack
+            // 
+            this.btbBack.BackColor = System.Drawing.Color.Red;
+            this.btbBack.FlatAppearance.BorderSize = 0;
+            this.btbBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btbBack.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btbBack.ForeColor = System.Drawing.Color.Black;
+            this.btbBack.Location = new System.Drawing.Point(12, 391);
+            this.btbBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btbBack.Name = "btbBack";
+            this.btbBack.Size = new System.Drawing.Size(73, 28);
+            this.btbBack.TabIndex = 12;
+            this.btbBack.Text = "<<Back";
+            this.btbBack.UseVisualStyleBackColor = false;
+            this.btbBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.LightGray;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.Black;
+            this.btnClear.Location = new System.Drawing.Point(338, 123);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(134, 29);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnAddUser.FlatAppearance.BorderSize = 0;
+            this.btnAddUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddUser.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddUser.ForeColor = System.Drawing.Color.Black;
+            this.btnAddUser.Location = new System.Drawing.Point(185, 123);
+            this.btnAddUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(134, 29);
+            this.btnAddUser.TabIndex = 10;
+            this.btnAddUser.Text = "Remove";
+            this.btnAddUser.UseVisualStyleBackColor = false;
+            this.btnAddUser.Click += new System.EventHandler(this.btnRemoveUser_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(275, 71);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(232, 27);
+            this.txtUsername.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(148, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "UserName";
+            // 
+            // userGrid
+            // 
+            this.userGrid.AllowUserToAddRows = false;
+            this.userGrid.AllowUserToDeleteRows = false;
+            this.userGrid.AllowUserToResizeRows = false;
+            this.userGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.userGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UserName,
+            this.Role});
+            this.userGrid.GridColor = System.Drawing.Color.Khaki;
+            this.userGrid.Location = new System.Drawing.Point(133, 226);
+            this.userGrid.Name = "userGrid";
+            this.userGrid.ReadOnly = true;
+            this.userGrid.Size = new System.Drawing.Size(399, 150);
+            this.userGrid.TabIndex = 14;
+            // 
+            // UserName
+            // 
+            this.UserName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.UserName.DataPropertyName = "UserName";
+            this.UserName.HeaderText = "User Name";
+            this.UserName.Name = "UserName";
+            this.UserName.ReadOnly = true;
+            // 
+            // Role
+            // 
+            this.Role.DataPropertyName = "Role";
+            this.Role.HeaderText = "Role";
+            this.Role.Name = "Role";
+            this.Role.ReadOnly = true;
+            // 
+            // FormRemoveUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 449);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FormRemoveUser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormRemoveUser";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormRemoveUser_FormClosed);
+            this.Load += new System.EventHandler(this.FormRemoveUser_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.userGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btbBack;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView userGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Role;
+    }
+}
---
+++ WindowsFormsAppLoginRegistration/FormRemoveUser.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppLoginRegistration
+{
+    public partial class FormRemoveUser : Form
+    {
+        private FormAdmin FormAdmin { get; set; }
+        private DataAccess Da { get; set; }
+        public FormRemoveUser()
+        {
+            InitializeComponent();
+            this.Da = new DataAccess();
+
+            this.PopulateGridView();
+        }
+        public FormRemoveUser(FormAdmin formAdmin) : this()
+        {
+            this.FormAdmin = formAdmin;
+        }
+
+        private void PopulateGridView()
+        {
+            string query = "select * from UserInformation";
+            var ds = this.Da.ExecuteQuery(query);
+
+            this.userGrid.DataSource = ds.Tables[0];
+        }
+        private void FormRemoveUser_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Hide();
+            this.FormAdmin.Show();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.FormAdmin.Show();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.txtUsername.Clear();
+            
+        }
+
+        public bool IsExist()
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=DESKTOP-A7UFOMU\SQLEXPRESS;Initial Catalog=LoginDatabase;Persist Security Info=True;User ID=sa;Password=tahsin1122");
+            sqlcon.Open();
+            string query = "select * from UserInformation where UserName = '" + this.txtUsername.Text + "';";
+            SqlCommand sqlCommand = new SqlCommand(query, sqlcon);
+            SqlDataAdapter sda = new SqlDataAdapter(sqlCommand);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+
+            if (ds.Tables[0].Rows.Count == 1)
+            {
+
+                sqlcon.Close();
+                return true;
+            }
+            else
+            {
+                sqlcon.Close();
+                return false;
+            }
+        }
+        public void RemoveUser()
+        {
+            try
+            {
+                /*SqlConnection sqlcon = new SqlConnection(@"Data Source=DESKTOP-A7UFOMU\SQLEXPRESS;Initial Catalog=LoginDatabase;Persist Security Info=True;User ID=sa;Password=tahsin1122");
+                sqlcon.Open();
+                string query = "delete from UserInformation where UserName=@UserName;";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlcon);
+                sqlCommand.Parameters.AddWithValue("@UserName", username);
+                sqlCommand.ExecuteNonQuery();
+                sqlcon.Close();
+                */
+                var sql = "delete from UserInformation where UserName='"+this.txtUsername.Text+"';";
+                this.Da.ExecuteQuery(sql);
+            }
+            catch
+            {
+                Exception ex = new Exception();
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void btnRemoveUser_Click(object sender, EventArgs e)
+        {
+            if(this.IsExist())
+            {
+                this.RemoveUser();
+                MessageBox.Show("User Removed");
+                this.PopulateGridView();
+
+            }
+            else
+                MessageBox.Show("No user found");
+
+        }
+
+        private void FormRemoveUser_Load(object sender, EventArgs e)
+        {
+            this.userGrid.AutoGenerateColumns=false;
+        }
+    }
+}
